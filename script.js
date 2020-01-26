5e6b57b73946a19ff43e950ec5cdaf0de04e937c
kcbobbe <kcbobbe@gmail.com>
2020-01-26 18:48:46 -0500

    Added a sorting algorithm for showing the high scores in order

--- script.js
+++ script.js
@@ -1,6 +1,53 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-// 04-09 has an example of a countdown
+  var questions = [
+    {question: "Which of the following HTML elements DOES NOT have default styling?",
+    answers: [
+      ["<h1>", false],["<p>",false],["<div>", true],["<ul>",false]]
+    },
+    {question: "Which of the following can be used to dynamically change styles in response to user input?",
+    answers: [
+      ["CSS", false], ["HTML", false], ["Java", false], ["JavaScript", true]]},
+    {question: "Which of the following values is falsy?",
+    answers: [
+      ["true", false], ["12", false], ["0", true], ["hello", false]]},
+    {question: "var sum1 ='3' + 3; var sum2 = 3 + 2 + '3' + 3;  what is the value of var1 and var2?",
+    answers: [
+      ["'33' and '3233'", false], ["'6' and '11", false], ["'6' and '533'", false], ["'33' and '533'", true]]},
+    {question: "Which of the following is a reason why jQuery might be used? (Choose the best answer)",
+    answers: [
+      ["Makes it easier to manipulate the DOM", false], 
+      ["Simplifies cross-browser compatibility", false], 
+      ["Can be used to add animations and effects to the page.", false], 
+      ["All of the above.", true]]},
+    {question: "Which of the following is a JavaScript object?",
+    answers: [
+      ["[6, 8, 9]", false], 
+      ["{name: 'Bob', age: '20', favoriteFood: 'pizza'}", true], 
+      ["[[0,2],[9,'cat']]", false],
+      ["All of the above.", false]]},
+    {question: "Which of the following refers to 'scope' in JavaScript (pick the best answer)",
+    answers: [
+      ["Scope refers to all of the parts of the web application that is accessible by the user", false], 
+      ["Scope refers to the hierachy of CSS selectors.", false], 
+      ["Scope refers to how the accessibility of a variable to different blocks of code depends on where the variables are initialized.", true], 
+      ["Scope refers to the complexity and volume of code that needs to be written in order to get a working prototype.", false]]},
+    {question: "Which of the following is an inline element?",
+    answers: [["<div>", false], ["<span>", true], ["<h1>", false], ["<p>", false]]},
+    {question: "Which of the following is an example of event bubbling?",
+    answers: [
+      ["Clicking a button causes a function to be called in JavaScript.", false], 
+      ["The JavaScript loads only after the DOM content renders.", false], 
+      ["An event causes a function to run in an infinite loop.", false], 
+      ["Clicking a child element also triggers an event linked to a parent element", true]]},
+    {question: "What is a use of local storage?",
+    answers: [
+      ["To reliably store confidential data on a local computer, such as passwords.", false], 
+      ["To store data in a database for users to access from any device.", false], 
+      ["To store some information or preferences in the user's browser that will be saved even if the page is refreshed.", true], 
+      ["To store documents and downloads on your device.", false]]}
+  ]
+
 var countdownTimer = document.getElementById("countdown");
 var startButton = document.getElementById("startButton");
 var welcomeScreen = document.getElementById("welcomeScreen");
@@ -16,13 +63,18 @@ var highScoreForm = document.getElementById("highScoreForm")
 var highScoreParent = document.getElementById("highScoreParent")
 
 var playAgain = document.getElementById("playAgain")
+//Button that shows the high scores view
 var viewHighScores = document.getElementById("viewHighScores")
-
+//view that is shown when the quiz is completed
 var endOfQuiz = document.getElementById("endOfQuiz");
-var playerScore = document.getElementById("playerScore");
 
+// message showing the player's score
+var playerScore = document.getElementById("playerScore");
+//Text for Question 1, Question 2, etc
 var questionTitle = document.getElementById("questionTitle")
+//text for questions
 var codeQuestion = document.getElementById("codeQuestion");
+//buttons for answers
 var codeAnswer = document.getElementById("answerButtons")
 
 var back = document.getElementById("back");
@@ -30,6 +82,7 @@ var back = document.getElementById("back");
 var questionNum = 0;
 var score = 0;
 var endGame=false
+var secondsLeft = 59;
 
 if (!localStorage.getItem("highScores")){
   var highScores = [];
@@ -38,66 +91,11 @@ if (!localStorage.getItem("highScores")){
   localStorage.setItem("highScores", JSON.stringify(highScores))
 }
 
-var sortedHighScores = [];
-
-var questions = [
-  {question: "Which of the following HTML elements DOES NOT have default styling?",
-  answers: [
-    ["<h1>", false],["<p>",false],["<div>", true],["<ul>",false]]
-  },
-  {question: "Which of the following can be used to dynamically change styles in response to user input?",
-  answers: [
-    ["CSS", true], ["HTML", false], ["Java", false], ["JavaScript", true]]},
-  {question: "Which of the following values is falsy?",
-  answers: [
-    ["true", false], ["12", false], ["0", true], ["hello", false]]},
-  {question: "var sum1 ='3' + 3; var sum2 = 3 + 2 + '3' + 3;  what is the value of var1 and var2?",
-  answers: [
-    ["'33' and '3233'", false], ["'6' and '11", false], ["'6' and '533'", false], ["'33' and '533'", true]]},
-  {question: "Which of the following is a reason why jQuery might be used? (Choose the best answer)",
-  answers: [
-    ["Makes it easier to manipulate the DOM", false], 
-    ["Simplifies cross-browser compatibility", false], 
-    ["Can be used to add animations and effects to the page.", false], 
-    ["All of the above.", true]]},
-  {question: "Which of the following is a JavaScript object?",
-  answers: [
-    ["[6, 8, 9]", false], 
-    ["{name: 'Bob', age: '20', favoriteFood: 'pizza'}", true], 
-    ["[[0,2],[9,'cat']]", false],
-    ["All of the above.", false]]},
-  {question: "Which of the following refers to 'scope' in JavaScript (pick the best answer)",
-  answers: [
-    ["Scope refers to all of the parts of the web application that is accessible by the user", false], 
-    ["Scope refers to the hierachy of CSS selectors.", false], 
-    ["Scope refers to how the accessibility of a variable to different blocks of code depends on where the variables are initialized.", true], 
-    ["Scope refers to the complexity and volume of code that needs to be written in order to get a working prototype.", false]]},
-  {question: "Which of the following is an inline element?",
-  answers: [["<div>", false], ["<span>", true], ["<h1>", false], ["<p>", false]]},
-  {question: "Which of the following is an example of event bubbling?",
-  answers: [
-    ["Clicking a button causes a function to be called in JavaScript.", false], 
-    ["The JavaScript loads only after the DOM content renders.", false], 
-    ["An event causes a function to run in an infinite loop.", false], 
-    ["Clicking a child element also triggers an event linked to a parent element", true]]},
-  {question: "What is a use of local storage?",
-  answers: [
-    ["To reliably store confidential data on a local computer, such as passwords.", false], 
-    ["To store data in a database for users to access from any device.", false], 
-    ["To store some information or preferences in the user's browser that will be saved even if the page is refreshed.", true], 
-    ["To store documents and downloads on your device.", false]]}
-]
-
-
-var secondsLeft = 59;
-
-
 
 function startQuiz(){
   flashFeedbackFalse.setAttribute("style", "display:none");
   flashFeedbackTrue.setAttribute("style", "display:none");
   inputInitials.removeAttribute("disabled");
-
 
   endGame=false;
   score=0;
@@ -113,7 +111,6 @@ function countdown(){
   var countdownInterval = setInterval(function(){
     if(endGame){
       clearInterval(countdownInterval);
-      console.log("ITS OVER");
       countdownTimer.innerText= "Clear time: " + secondsLeft + " seconds";
     } else {
       countdownTimer.innerText= "Time Remaining: " + secondsLeft + " seconds";
@@ -125,8 +122,6 @@ function countdown(){
         endOfQuiz.setAttribute("style", "display: block")
         var scorePercent = (Math.floor((score / questions.length) * 100)) 
         playerScore.innerText = "You ran out of time. Your final score is: " + scorePercent + "%";
-        // highScores.push(score);
-        // localStorage.setItem("highScores", highScores);
         clearInterval(countdownInterval);
       }
       secondsLeft--;
@@ -140,7 +135,6 @@ function flashFeedback(correct){
     flashFeedbackFalse.setAttribute("style", "display:none");
     flashFeedbackTrue.setAttribute("style", "display:block");
     setTimeout(function(){
-      // flashFeedbackTrue.setAttribute("style", "opacity:0");
       flashFeedbackTrue.setAttribute("style", "display:none");
 
     }, 1000)
@@ -149,7 +143,6 @@ function flashFeedback(correct){
     flashFeedbackTrue.setAttribute("style", "display:none");
     flashFeedbackFalse.setAttribute("style", "display:block");
     setTimeout(function(){
-      // flashFeedbackFalse.setAttribute("style", "opacity:0");
       flashFeedbackFalse.setAttribute("style", "display: none");
 
     }, 1000)
@@ -159,18 +152,11 @@ function flashFeedback(correct){
 function displayQuestion(){
   welcomeScreen.setAttribute("style", "display:none")
   quizScreen.setAttribute("style", "display:block");
-
-  // for (i = 0; i < questions.length; i++){
-    questionTitle.innerText= ("Question " + (questionNum + 1) );
-    codeQuestion.innerText = questions[questionNum].question;
-    for (i=0; i<codeAnswer.children.length; i++){
-      codeAnswer.children[i].innerText = questions[questionNum].answers[i][0];
-    }
-    // codeAnswerA.innerText = questions[0].answers[0][0]
-    // codeAnswerB.innerText = questions[0].answers[1][0]
-    // codeAnswerC.innerText = questions[0].answers[2][0]
-    // codeAnswerD.innerText = questions[0].answers[3][0]
-  // }
+  questionTitle.innerText= ("Question " + (questionNum + 1) );
+  codeQuestion.innerText = questions[questionNum].question;
+  for (i=0; i<codeAnswer.children.length; i++){
+    codeAnswer.children[i].innerText = questions[questionNum].answers[i][0];
+  }
 }
 
 function checkAnswer(e){
@@ -194,9 +180,6 @@ function checkAnswer(e){
         playerScore.innerText = "Your final score is: " + scorePercent+ "%";
         countdownTimer.innerText= "Clear time: " + secondsLeft + " seconds";
         endGame=true;
-        // highScores.push(scorePercent);
-        // localStorage.setItem("highScores", highScores);
-        // console.log(score)
       }
 
     } else {
@@ -215,8 +198,6 @@ function checkAnswer(e){
         endOfQuiz.setAttribute("style", "display: block")
         var scorePercent = (Math.floor((score / questions.length) * 100)) 
         playerScore.innerText = "Your final score is: " + scorePercent + "%";
-        // highScores.push(scorePercent);
-        // localStorage.setItem("highScores", highScores);
         console.log(highScores)
       }
   }
@@ -226,21 +207,13 @@ function logHighScore(e){
   e.preventDefault();
   var scorePercent = (Math.floor((score / questions.length) * 100)) 
   highScores.push({score:scorePercent,initials: inputInitials.value});
+  highScores = highScores.sort(compare);
   localStorage.setItem("highScores",JSON.stringify(highScores))
-  console.log(highScores)
   inputInitialsButton.setAttribute("disabled","")
   inputInitials.setAttribute("disabled","")
   inputInitialsButton.setAttribute("class", "btn btn-secondary");
-  // inputInitialsButton.removeAttribute("btn-secondary")
-}
-  // if (questions[0].answers[i][0]===true){
-  //   score = score + 1;
-  //   console.log("true");
-  //   // displayQuestion()
-  // } else {
-  //   console.log('false')
-  // //   displayQuestion()
-  // }
+  }
+ 
 
   function checkInitialsInput() {
     if (inputInitials.value){
@@ -253,16 +226,8 @@ function logHighScore(e){
   function seeHighScores(){
     endOfQuiz.setAttribute("style","display:none");
     highScoresView.setAttribute("style","display: block");
-    // console.log(highScores, "high scores before findMax")
-    // console.log(sortedHighScores, "sorted Array")
-    // findMax(highScores);
-    // console.log(highScores, "high scores after find max")
-    // console.log(sortedHighScores, "sorted array after find max")
-    // for (i = 0; i < highScoreParent.childNodes.length; i++){
-    //   highScoreParent.removeChild(highScoreParent.childNodes[i]);
-    // }
     highScoreParent.innerHTML = "";
-    for (i = 0; i < highScores.length; i++){
+    for (var i = 0; i < highScores.length; i++){
       // console.log(highScores[i])
       var newTableRow= document.createElement('tr');
       var newRankingCell = document.createElement('th');
@@ -279,46 +244,26 @@ function logHighScore(e){
 
     }
   }
-
-  // function orderHighScores(){
-  //   for (i=0; i < highScores.length; i++){
-  //     findMax(highScore[i])
-  //   }
-    
-
-  //   }
-
-
-  function findMax(scoreArray){
-    var max = [0, 0]
-    for (i=0; i <= scoreArray.length; i++){
-      if (scoreArray[i].score > max[0]){
-        max = [scoreArray[i].score, i]
-        console.log(max, "max")
-      }
-    }
-    var topScore = (scoreArray.splice(max[1], 1))
-    console.log(topScore, "here is the calculated top score...")
-    sortedHighScores.push(topScore[0])
-    if (scoreArray.length > 0){
-      findMax(scoreArray);
-    } else {
-      highScores =  sortedHighScores;
-    }
+//sorting high score list from high score to low score
+// https://www.sitepoint.com/sort-an-array-of-objects-in-javascript/
+  function compare(a, b) {
+    if (a.score < b.score) return 1;
+    if (b.score < a.score) return -1;
+    return 0;
   }
 
+  //Clearing array of high scores, in local storage
   function clear(){
     localStorage.clear();
     highScores=[];
     highScoreParent.innerHTML="";
   }
 
+  //Reseting quiz and showing home page view
   function backToMain(){
     countdownTimer.innerText= "Time Remaining: 60 seconds";
-
     highScoresView.setAttribute("style","display:none");
     welcomeScreen.setAttribute("style", "display:block");
-
   }
 
 highScoreForm.addEventListener('submit', logHighScore)
@@ -326,13 +271,13 @@ highScoreForm.addEventListener('submit', logHighScore)
 playAgain.addEventListener("click", startQuiz)
 
 startButton.addEventListener("click", startQuiz)
-// codeAnswerA.addEventListener("click", checkAnswer, 0)
+
 codeAnswer.addEventListener("click", checkAnswer)
-
+//text input for entering initials
 inputInitials.addEventListener("input", checkInitialsInput)
-
+//view high scores button
 viewHighScores.addEventListener("click", seeHighScores)
-
+//clear high scores button
 clearLocalStorage.addEventListener("click", clear)
 
 back.addEventListener("click",backToMain)
